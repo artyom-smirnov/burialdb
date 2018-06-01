f8a62e39f56ec5cec6de67ae01064a61f753a30c
Artyom Smirnov <artyom_smirnov@me.com>
2018-06-01 12:56:50 +0300

    Simple tests

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
-# Create your tests here.
+from website.models import Person
+
+
+class PersonTestCase(TestCase):
+    def setUp(self):
+        Person.objects.create()
+        Person.objects.create(fio='a')
+        Person.objects.create(fio='b', fio_actual='c')
+
+    def test_unknown_person_name(self):
+        person = Person.objects.get(fio='')
+        self.assertEqual(person.name(), 'Неизвестный')
+
+    def test_unknown_person_status(self):
+        person = Person.objects.get(fio='')
+        self.assertEqual(person.get_status(), Person.INCOMPLETE)
+
+    def test_incomplete_person_name(self):
+        person = Person.objects.get(fio='a')
+        self.assertEqual(person.name(), 'a')
+
+    def test_incomplete_person_status(self):
+        person = Person.objects.get(fio='a')
+        self.assertEqual(person.get_status(), Person.INCOMPLETE)
+
+    def test_partial_person_name(self):
+        person = Person.objects.get(fio='b')
+        self.assertEqual(person.name(), 'c')
+
+    def test_partial_person_status(self):
+        person = Person.objects.get(fio='b')
+        self.assertEqual(person.get_status(), Person.PARTIAL)
+
+    def tearDown(self):
+        Person.objects.all().delete()
