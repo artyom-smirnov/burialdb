bb2f076680ce35866b006e0986c0693824b96f18
Artyom Smirnov <artyom_smirnov@me.com>
2018-06-05 17:14:05 +0300

    Make pagination more common

--- website/static/website/scripts.js
+++ website/static/website/scripts.js
@@ -3,3 +3,30 @@ $( document ).ready(function() {
         window.location = $(this).data("href");
     });
 });
+
+
+function activate_pagination(page, load_more_url, list_element) {
+    var current_page = page;
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() === $(document).height() - $(window).height()) {
+            load_more();
+        }
+    });
+
+    function load_more() {
+        $.ajax({
+            url: load_more_url,
+            data: {"page": current_page + 1},
+            contentType: "application/json",
+            success: function (data) {
+                current_page += 1;
+                list_element.after(data.content);
+            },
+            error: function () {
+                load_more = function () {
+                }
+            }
+        });
+    }
+}
